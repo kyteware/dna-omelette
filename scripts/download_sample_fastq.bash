aa58809a96da627bab09349c49b7425cb33b5c05
Dworv <dwarvyt@gmail.com>
2022-07-28 22:17:53 -0700

    feat: change output file name to sample.fastq

--- scripts/download_sample_fastq.bash
+++ scripts/download_sample_fastq.bash
@@ -2,7 +2,7 @@
 echo $1
 if [[ $1 != "" ]]
 then
-    wget -P $1 "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
+    wget -P $1 -O sample.fastq "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
 else
-    wget "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
+    wget -O sample.fastq "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
 fi
