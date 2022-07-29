b84c003783b92141a68325453eebfdbcbbed210f
Dworv <dwarvyt@gmail.com>
2022-07-29 09:03:05 -0700

    feat: only download if file doesnt already exist

--- scripts/download_sample_fastq.bash
+++ scripts/download_sample_fastq.bash
@@ -1,7 +1,23 @@
 #!/bin/bash
-if [[ $1 != "" ]]
+
+if [[ $1 == "" ]]
 then
-    wget -q -P $1 -O sample.fastq "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
+    FILEPATH="./"
 else
-    wget -q -O sample.fastq "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
+    LASTLETTER=${1: -1}
+    if [[ $LASTLETTER != "/" ]]
+    then
+        FILEPATH="$1/"
+    else
+        FILEPATH=$1
+    fi
+fi
+
+if test -f "$FILEPATH/sample.fastq"
+then
+    echo "Sample FASTQ file already downloaded, skipping..."
+else
+    echo "FILEPATH B4 DOWNLOAD: $FILEPATH"
+    wget -O "$FILEPATH/sample.fastq" "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
+    echo "Sample FASTQ file downloaded..."
 fi
