66bd99e7a895d7e503a72bea675b94b82dbac2a4
Dworv <dwarvyt@gmail.com>
2022-07-28 22:03:46 -0700

    feat: allow path input for fastq download script

--- scripts/download_sample_fastq.bash
+++ scripts/download_sample_fastq.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
-wget https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1
+echo $1
+if [[ $1 != "" ]]
+then
+    wget -P $1 "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
+else
+    wget "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
+fi
