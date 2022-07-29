2c938f4b2183294b4ea4b8fcafaf7a2ecd2611f0
Dworv <dwarvyt@gmail.com>
2022-07-28 22:33:45 -0700

    feat: make wget quite and remove debug echo

--- scripts/download_sample_fastq.bash
+++ scripts/download_sample_fastq.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo $1
 if [[ $1 != "" ]]
 then
-    wget -P $1 -O sample.fastq "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
+    wget -q -P $1 -O sample.fastq "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
 else
-    wget -O sample.fastq "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
+    wget -q -O sample.fastq "https://zenodo.org/record/3736457/files/1_control_psbA3_2019_minq7.fastq?download=1"
 fi
