9213caafc17c66a4b0aa0164691e81cdc41749ab
Dworv YT <dwarvyt@gmail.com>
2022-08-23 22:56:15 -0400

    feat: change desequencer to more accurately represent dna readers

--- analysis/desequencer.jl
+++ analysis/desequencer.jl
@@ -1,40 +1,60 @@
 
 function getargs()
     if length(ARGS) >= 1
-        readlen = ARGS[1]
+        readnum = parse(Int64, ARGS[1])
     else
-        readlen = 50
+        readnum = 100
     end
     if length(ARGS) >= 2
-        overlap = ARGS[2]
+        low, high = split(ARGS[2], ":")
+        readlenrange = range(parse(Int64, low), parse(Int64, high), step=1)
     else
-        overlap = 20
+        readlenrange = 20:50
     end
 
-    return readlen, overlap
+    return readnum, readlenrange
+end
+
+function reverse_sequence(seq)
+    newseq = ""
+    for nucleo in reverse(seq)
+        if nucleo == 'A'
+            newseq *= 'T'
+        elseif nucleo == 'T'
+            newseq *= 'A'
+        elseif nucleo == 'G'
+            newseq *= 'C'
+        else
+            newseq *= 'G'
+        end
+    end
+    return newseq
 end
 
 function main()
     run(`./scripts/download_sample_fasta.bash`)
-    # overlaps not implemented yet
-    readlen, overlap = getargs()
-    sample = read("./sample.fasta", String)
-    fasta = IOBuffer(sample)
-
+    readnum, readlenrange = getargs()
     reads = Vector{String}()
-    currentread = Vector{Char}()
-    countdown = 0
-    for letter in fasta
-        if peek(fasta) === nothing
-            countdown = 0
-        end
 
-        if countdown == 0
-            append!(reads, join(currentread))
-            countdown = rand(Int, readlen*0.5, readlen*1.5)
+    # left = rand(round(readnum*0.25) : round(readnum*0.75))
+    # right = length(sample) - left
+
+    sequence = ""
+    for line in readlines("./sample.fasta")
+        if !startswith(line, ">")
+            sequence *= strip(line)
+        end
+    end
+
+    for _ in 1:readnum
+        pos = rand(1:length(sequence))
+        toright = rand(Bool)
+        if toright
+            readrange = pos:min(pos + rand(readlenrange), length(sequence))
+            push!(reads, sequence[readrange])
         else
-            countdown -= 1
-            append!(currentread, letter)
+            readrange = max(pos - rand(readlenrange), 1):pos
+            push!(reads, reverse_sequence(sequence[readrange]))
         end
     end
     println(reads)
