b3b7f8dc441c437cd11fa45b2527d5504fd54f7d
Dworv <dwarvyt@gmail.com>
2023-04-16 22:23:48 -0700

    feat: create a histogram of kmers

---
+++ analysis/hist_kmers.jl
@@ -0,0 +1,45 @@
+import Pkg
+Pkg.add("Dictionaries")
+
+using Plots, Dictionaries
+
+run(`./scripts/download_sample_fasta.bash`)
+
+if length(ARGS) >= 1
+    klength = parse(Int64, ARGS[1])
+else
+    klength = 1
+end
+
+filename = "./sample.fastq"
+kmers = Dict{String,Int64}()
+
+lines_until_read = 1
+for line in eachline(filename)
+    global lines_until_read
+    if lines_until_read == 0
+        line = strip(line)
+        for i in 1:(length(line)-klength)
+            kmer = SubString(line, i:(i+klength))
+            if haskey(kmers, kmer)
+                kmers[kmer] += 1
+            else
+                kmers[kmer] = 1
+            end
+        end
+        lines_until_read = 3
+    else
+        lines_until_read -= 1
+    end
+end
+
+# prints the output dict
+vals = collect(values(kmers))
+
+println(vals)
+
+# tries to display a graph (takes forever for some reason)
+display(histogram(vals))
+
+# apparently this stops the display from immediatly closing
+readline()
