bd2d954e8ddfd28c50de9425a03b6e67055ecbeb
Dworv <dwarvyt@gmail.com>
2023-06-23 11:32:34 -0700

    feat: kmer tool to read file and output csv of kmers

---
+++ kmer-tool/src/lib.rs
@@ -0,0 +1,38 @@
+use std::{io::{Read, BufReader}, collections::HashMap};
+
+use utf8_chars::BufReadCharsExt;
+
+type Kmers = HashMap<Vec<char>, u32>;
+
+pub fn get_kmers<R: Read>(k: usize, seq: &mut BufReader<R>) -> Result<Kmers, KmerError> {
+    let mut kmers: Kmers = HashMap::new();
+    let mut kmer: Vec<char> = Vec::with_capacity(k);
+    let mut chars = seq.chars().map(|c| c.unwrap());
+
+    // make the first element of kmer empty to allow being slid left
+    kmer.push(' ');
+    // fill kmer with first k chars
+    kmer.append(&mut chars.by_ref().take(k-1).collect());
+    if kmer.len() < k {
+        return Err(KmerError::NotEnoughChars);
+    }
+
+    // iterate over the rest of the chars
+    for c in chars {
+        kmer.rotate_left(1);
+        kmer[k-1] = c;
+        let count = kmers.entry(kmer.clone()).or_insert(0);
+        *count += 1;
+    }
+
+    if kmers.len() == 0 {
+        return Err(KmerError::NotEnoughChars);
+    }
+
+    Ok(kmers)
+}
+
+#[derive(Debug)]
+pub enum KmerError {
+    NotEnoughChars,
+}
---
+++ kmer-tool/src/main.rs
@@ -0,0 +1,44 @@
+use std::{fs::File, io::{Read, BufReader}, path::PathBuf, process::exit};
+
+use clap::Parser;
+use csv::Writer;
+
+use kmer_tool::get_kmers;
+
+#[derive(Debug, Parser)]
+#[clap(author, version)]
+struct Cli {
+    /// This tool usually doesn't allow for k sizes large than 16, but this flag will allow it.
+    #[clap(short, long)]
+    force_k: bool,
+    /// The size of the kmer to be counted.
+    #[clap(short, default_value = "1")]
+    k: usize,
+    /// The file to read from.
+    #[clap()]
+    input: PathBuf,
+    /// The file to write to.
+    #[clap()]
+    output: PathBuf
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let k = cli.k;
+    if k > 8 && !cli.force_k {
+        eprintln!("K mut be less than or equal to 16. Use the -f flag to ignore this rule");
+        exit(1);
+    }
+
+    let mut input = BufReader::new(File::open(cli.input).unwrap());
+    let kmers = get_kmers(k, &mut input).unwrap();
+
+    let mut output = File::create(cli.output).unwrap();
+    let mut writer = Writer::from_writer(&mut output);
+    writer.write_record(["item", "count"]).unwrap();
+    for (kmer, count) in kmers {
+        writer.write_record(&[kmer.iter().collect::<String>(), count.to_string()]).unwrap();
+    }
+    writer.flush().unwrap();
+}
